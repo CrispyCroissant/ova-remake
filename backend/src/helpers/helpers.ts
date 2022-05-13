871ef6396f8877936998e9615d54f1210202a1b4
CrispyCroissant <gentlemug00@gmail.com>
2022-05-13 15:37:50 +0200

    Create helper function for creating email bodies

--- backend/src/helpers/helpers.ts
+++ backend/src/helpers/helpers.ts
@@ -1,3 +1,6 @@
+import { Customer, Fare } from '../../../shared/types';
+import { OrderStatus } from '../types/orderStatuses';
+
 function priceCalc(distance: number): number {
   const distanceKm = Math.round(distance / 1000);
   const flatRate = 1000; // in SEK.
@@ -15,4 +18,34 @@ function priceCalc(distance: number): number {
   return price;
 }
 
-export { priceCalc };
+function createMessage(
+  customer: Customer,
+  fare: Fare,
+  status: OrderStatus
+): string {
+  const { firstName, lastName, address, city, email, phone } = customer;
+  const { from, to, price, tax, date, distance } = fare;
+
+  return `
+    Beställning
+
+    Status: ${status}
+    Pris: ${price} (moms: ${tax})
+    
+    Transportinformation:
+      Från: ${from}
+      Till: ${to}
+      Distans: ${distance / 1000} km
+      Datum: ${date}
+      
+    Kundinformation:
+      Org-nummer: ${customer.regNumber ? customer.regNumber : '-'}
+      Namn: ${firstName} ${lastName}
+      Adress: ${address}
+      Ort: ${city}
+      E-post: ${email}
+      Telefonnummer: ${phone}
+`;
+}
+
+export { priceCalc, createMessage };
