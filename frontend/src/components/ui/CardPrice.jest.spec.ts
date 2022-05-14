e47bd8971a939a3b4716bf8101f0bcf545ac83a0
CrispyCroissant <gentlemug00@gmail.com>
2022-05-14 15:03:32 +0200

    Update tests

--- frontend/src/components/ui/CardPrice.jest.spec.ts
+++ frontend/src/components/ui/CardPrice.jest.spec.ts
@@ -24,44 +24,11 @@ describe('CardPrice', () => {
     });
   });
 
-  it("shows the first 'from' and last 'to' in the trips as 'Vårt garage'", async () => {
-    store.order.trips = [
-      { from: 'Place1', to: 'Place2', distance: 100 },
-      { from: 'Place3', to: 'Place4', distance: 200 },
-      { from: 'Place5', to: 'Place6', distance: 300 },
-    ];
-    await wrapper.vm.$nextTick();
-
-    const trips = wrapper.findAll('.q-breadcrumbs__el');
-
-    expect(trips[0].text()).toBe('Vårt garage');
-    expect(trips[1].text()).toBe('Place2');
-    expect(trips[2].text()).toBe('Place3');
-    expect(trips[3].text()).toBe('Place4');
-    expect(trips[4].text()).toBe('Place5');
-    expect(trips[5].text()).toBe('Vårt garage');
-  });
-
-  it('shows the distance for each trip', async () => {
-    store.order.trips = [
-      { from: 'Place1', to: 'Place2', distance: 150400 },
-      { from: 'Place3', to: 'Place4', distance: 223400 },
-      { from: 'Place5', to: 'Place6', distance: 331200 },
-    ];
-    await wrapper.vm.$nextTick();
-
-    const distances = wrapper.findAll('.trip-dist');
-
-    expect(distances[0].text()).toBe('150 km');
-    expect(distances[1].text()).toBe('223 km');
-    expect(distances[2].text()).toBe('331 km');
-  });
-
   it('shows the total price for the transport', async () => {
     store.order.priceTotal = 1234;
     await wrapper.vm.$nextTick();
 
-    expect(wrapper.find('#price').text()).toBe('Totalt: 1\xa0234\xa0kr');
+    expect(wrapper.find('#price').text()).toBe('1\xa0234\xa0kr');
   });
 
   it('shows the total transport distance', async () => {
@@ -72,17 +39,24 @@ describe('CardPrice', () => {
   });
 
   it('does not display any zip codes', async () => {
-    store.order.trips = [
-      { from: 'Place1', to: '893 31 Bjästa', distance: 1000 },
-      { from: 'Place3', to: '103 21 Rovers', distance: 2000 },
-    ];
+    store.order.from = '123 32 Place';
+    store.order.to = '443 12 Place2';
+
     await wrapper.vm.$nextTick();
 
-    const trips = wrapper.findAll('.q-breadcrumbs__el');
+    const locations = wrapper.find('#locs');
 
-    expect(trips[0].text()).toBe('Vårt garage');
-    expect(trips[1].text()).toBe('Bjästa');
-    expect(trips[2].text()).toBe('Place3');
-    expect(trips[3].text()).toBe('Rovers');
+    expect(locations.text()).toBe('Place - Place2');
+  });
+
+  it('shows a shorter location name if the full name is too long', async () => {
+    store.order.from = 'aaaaaaaaaaaa, Place, Sverige';
+    store.order.to = 'bbbbbbbbbbbb, Place2, Sverige';
+
+    await wrapper.vm.$nextTick();
+
+    const locations = wrapper.find('#locs');
+
+    expect(locations.text()).toBe('Place - Place2');
   });
 });
