019a12289ccedc55b63c3f5422431ea9aba32e1c
CrispyCroissant <gentlemug00@gmail.com>
2022-05-30 13:29:38 +0200

    Switch employee images to stock icons.

--- frontend/src/components/About/EmployeeSection.vue
+++ frontend/src/components/About/EmployeeSection.vue
@@ -6,20 +6,16 @@
 
     <div class="row full-width justify-evenly">
       <div class="q-mx-lg">
-        <q-avatar size="15vh" class="shadow-4">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/President_Barack_Obama.jpg/1200px-President_Barack_Obama.jpg"
-          />
+        <q-avatar size="15vh" color="white" class="shadow-4">
+          <q-icon name="mdi-account" color="secondary"></q-icon>
         </q-avatar>
         <p class="q-mt-md q-mb-none text-body1 text-center">Damian Edlund</p>
         <p class="q-mt-none text-overline text-center">Ägare</p>
       </div>
 
       <div class="q-mx-lg">
-        <q-avatar size="15vh" class="shadow-5">
-          <img
-            src="https://media.istockphoto.com/photos/portrait-of-a-chinese-guy-picture-id118225344?k=20&m=118225344&s=170667a&w=0&h=-YtvcW22zOcPRGvnTZLJM4lNGmtnFNaaPJwuD6gjZ40="
-          />
+        <q-avatar size="15vh" class="shadow-5" color="white">
+          <q-icon name="mdi-account" color="secondary"></q-icon>
         </q-avatar>
         <p class="q-mt-md q-mb-none text-body1 text-center">Uthen Sunphuan</p>
         <p class="q-mt-none text-overline text-center">Ägare</p>
